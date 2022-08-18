552a455cec7ac673731c0fe41fb5370bdc0dfe72
arifulhasantotul <mdahtotul@gmail.com>
2022-08-18 15:00:04 +0600

    navbar init

--- components/Navbar/UpperNavbar/UpperNavbar.js
+++ components/Navbar/UpperNavbar/UpperNavbar.js
@@ -1,9 +1,31 @@
+import { Container } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { useStateContext } from "../../../context/ContextProvider";
+import routes from "../../../routes/routes";
+import styles from "./UpperNavbar.module.css";
 
 const UpperNavbar = () => {
+  const { pageURL } = useStateContext();
+
   return (
-    <div>
-      <h2>This is upper navbar</h2>
+    <div className={styles.nav_sec}>
+      <Container maxWidth="lg">
+        <div className={styles.nav_brand}>
+          <Image src="" alt="" />
+        </div>
+        <div className={styles.nav_links}>
+          {routes.map((path, idx) => (
+            <span
+              key={idx}
+              className={pageURL === path.to ? styles.activeLink : styles.link}
+            >
+              <Link href={path.to}>{path.name}</Link>
+            </span>
+          ))}
+        </div>
+      </Container>
     </div>
   );
 };
