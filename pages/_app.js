ce0fcba31305eaa330971d40116d38b04e4c27e6
arifulhasantotul <mdahtotul@gmail.com>
2022-08-27 20:41:34 +0600

    added page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,44 @@
+import { AnimatePresence, motion } from "framer-motion";
 import "swiper/css/bundle";
 import Layout from "../components/Layout/Layout";
 import ContextProvider from "../context/ContextProvider";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     // context api wrapper
     <ContextProvider>
       {/* page layout  */}
       <Layout>
-        {/* page components */}
-        <Component {...pageProps} />
+        {/* page transition */}
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            exit="exitAnimation"
+            transition={{
+              duration: 0.75,
+            }}
+            variants={{
+              pageInitial: {
+                opacity: 0,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              pageAnimate: {
+                scale: 1,
+                opacity: 1,
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+              },
+              exitAnimation: {
+                clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+              },
+            }}
+          >
+            {/* page components */}
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
       </Layout>
     </ContextProvider>
   );
--- pages/home/HomePage.js
+++ pages/home/HomePage.js
@@ -1,9 +1,37 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
-      <h2>This is homepage</h2>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <p>PEACE BE UPON YOU</p>
+      </motion.div>
+      <h1>
+        Hi, I&apos;m <span>MD. ARIFUL HASAN</span>
+      </h1>
+      <p>
+        I&apos;m a self-motivated, innovative, task-driven web designer and
+        developer with a passion for web designing and development. I always try
+        to keep my work simple, clean and effective for other developers and
+        users.
+      </p>
     </div>
   );
 };
