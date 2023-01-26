d179a0830cfe7e9e517aa1c9e4fa59a61fd38540
arifulhasantotul <mdahtotul@gmail.com>
2023-01-26 21:52:12 +0600

    review working fine alhamdullilah

--- components/Slider/ReviewSlider.js
+++ components/Slider/ReviewSlider.js
@@ -1,39 +1,38 @@
 import Review from "@/components/ReviewComp/Review";
 import styles from "@/styles/ReviewSlider.module.css";
-import { Keyboard, Navigation, Pagination } from "swiper";
+import { Keyboard, Mousewheel, Navigation, Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-const ReviewSlider = ({ allReview }) => {
-  console.log(allReview);
-
+const ReviewSlider = ({ allReview, data }) => {
   return (
     <div className={styles.slider_wrapper}>
-      <Swiper
-        cssMode={true}
-        navigation={{
-          nextEl: ".custom_next",
-          prevEl: ".custom_prev",
-        }}
-        // mousewheel={true}
-        keyboard={true}
-        // grabCursor={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[
-          Navigation,
-          Pagination,
-          // Mousewheel,
-          Keyboard,
-        ]}
-        className="review_swiper"
-      >
-        {allReview.map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <Review review={item} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {allReview.length > 0 ? (
+        <Swiper
+          cssMode={true}
+          navigation={{
+            nextEl: ".custom_next",
+            prevEl: ".custom_prev",
+          }}
+          mousewheel={true}
+          keyboard={true}
+          grabCursor={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className="review_swiper"
+        >
+          {allReview.map((item, idx) => (
+            <SwiperSlide key={idx}>
+              <Review review={item} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : allReview.length === 0 ? (
+        <div>Not Found</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
