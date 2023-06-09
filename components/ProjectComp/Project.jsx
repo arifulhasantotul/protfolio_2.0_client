464caef030aef13f41daaed3b6af13bd6794fa30
mdahtotul <mdahtotul@gmail.com>
2023-06-09 15:40:57 +0600

    getting list project

---
+++ components/ProjectComp/Project.jsx
@@ -0,0 +1,10 @@
+const Project = ({ projects, accessToken }) => {
+  console.log("🚀 ~ file: Project.jsx:3 ~ Project ~ projects:", projects);
+  return (
+    <div>
+      <h2>This is project component</h2>
+    </div>
+  );
+};
+
+export default Project;
