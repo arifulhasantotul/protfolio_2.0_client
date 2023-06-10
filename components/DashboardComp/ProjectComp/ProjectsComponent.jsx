c3708bdab6bd1dc3336dff9c85de60bc9d8b8f2f
mdahtotul <mdahtotul@gmail.com>
2023-06-10 12:17:53 +0600

    dash projects, categories, tags added

---
+++ components/DashboardComp/ProjectComp/ProjectsComponent.jsx
@@ -0,0 +1,54 @@
+import PageHeader from "@/components/PageHeader/PageHeader";
+import { useStateContext } from "@/context/ContextProvider";
+import styles from "@/styles/ProjectsComponent.module.css";
+import { Container } from "@mui/material";
+import { MdDeleteOutline, MdEdit } from "react-icons/md";
+
+const ProjectsComponent = ({ projects }) => {
+  const { currentColor, darkTheme } = useStateContext();
+
+  // css conditionalMode for dark mode
+  const conditionalMode = darkTheme ? styles.dark : styles.light;
+
+  return (
+    <div className={`${conditionalMode} ${styles.projects_table_wrapper}`}>
+      <Container maxWidth="lg">
+        <PageHeader title="Projects" />
+
+        <div
+          className={styles.table_container}
+          style={{
+            color: currentColor,
+          }}
+        >
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th className={styles.t_data_center}>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(projects)
+                ? projects.map((project, idx) => (
+                    <tr key={idx}>
+                      <td>{project.name}</td>
+                      <td className={styles.t_data_center}>
+                        <MdEdit className={styles.icon} title="Edit project" />
+                        <MdDeleteOutline
+                          className={styles.icon}
+                          title="Delete project"
+                        />
+                      </td>
+                    </tr>
+                  ))
+                : null}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ProjectsComponent;
---
+++ components/DashboardComp/TagComp/TagsComponents.jsx
@@ -0,0 +1,55 @@
+import PageHeader from "@/components/PageHeader/PageHeader";
+import { useStateContext } from "@/context/ContextProvider";
+import styles from "@/styles/TagsComponent.module.css";
+import { Container } from "@mui/material";
+import { MdDeleteOutline, MdEdit } from "react-icons/md";
+
+const TagsComponent = ({ tags }) => {
+  console.log("🚀 ~ file: TagsComponents.jsx:7 ~ TagsComponent ~ tags:", tags);
+  const { currentColor, darkTheme } = useStateContext();
+
+  // css conditionalMode for dark mode
+  const conditionalMode = darkTheme ? styles.dark : styles.light;
+
+  return (
+    <div className={`${conditionalMode} ${styles.tags_table_wrapper}`}>
+      <Container maxWidth="lg">
+        <PageHeader title="Tags" />
+
+        <div
+          className={styles.table_container}
+          style={{
+            color: currentColor,
+          }}
+        >
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th className={styles.t_data_center}>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(tags)
+                ? tags.map((tag, idx) => (
+                    <tr key={idx}>
+                      <td>{tag.name}</td>
+                      <td className={styles.t_data_center}>
+                        <MdEdit className={styles.icon} title="Edit tag" />
+                        <MdDeleteOutline
+                          className={styles.icon}
+                          title="Delete tag"
+                        />
+                      </td>
+                    </tr>
+                  ))
+                : null}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default TagsComponent;
